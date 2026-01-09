29577a9748bba9f78c68a65e6d364aec8038c21e
GageOLankford <137559956+GageOLankford@users.noreply.github.com>
2026-01-09 11:34:42 +0200

    image saver example code upload

---
+++ image saver example/src/image_saver/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'imager_saver'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('lib/'+package_name,['image_saver/image_saver.py'])
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'image_saver = imager_saver.image_saver:main'
+        ],
+    },
+)
