0e5f8144fdc730f599d170410dac68c870b7ce93
MariaSotiriouSigma <sigoulakim@gmail.com>
2026-01-28 14:39:35 +0200

    blue...

--- image_saver_example/image_saver/image_saver/image_saver.py
+++ image_saver_example/image_saver/image_saver/image_saver.py
@@ -4,6 +4,9 @@ import os
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import CompressedImage
+import cv2
+import numpy as np
+from cv_bridge import CvBridge
 
 
 class ImageSaver(Node):
@@ -12,17 +15,39 @@ class ImageSaver(Node):
         self.output_dir = "/workspace/images/"
         os.makedirs(self.output_dir, exist_ok=True)
         self.vehicle_name = os.getenv('VEHICLE_NAME')
-        self.counter = 0
-        self.create_subscription(CompressedImage, f'/{self.vehicle_name}/image/compressed', self.save_image, 10)
-
-    def save_image(self, msg):
-        if self.counter % 30 != 0:
-            self.counter += 1
+        if self.vehicle_name is None:
+            self.get_logger().error("VEHICLE_NAME not set")
             return
-        with open(self.output_dir + str(self.counter) + '.jpg', 'wb') as f:
-            self.get_logger().info(f'Saving image {self.counter}')
-            f.write(msg.data)
-        self.counter += 1
+        self.counter = 0
+        self.create_subscription(CompressedImage, f'/{self.vehicle_name}/image/compressed', self.callback, 10)
+
+    def callback(self, msg):
+        # Convert compressed image to OpenCV format
+        np_arr = np.frombuffer(msg.data, np.uint8)
+        frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+        # Convert to HSV
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        # Blue mask
+        lower_blue = np.array([100, 150, 50])
+        upper_blue = np.array([140, 255, 255])
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+        #debugging
+        cv2.imshow("Blue Mask", mask)
+        cv2.waitKey(1)
+
+        # How much blue is present?
+        blue_pixels = cv2.countNonZero(mask)
+
+        if blue_pixels > 5000:  # threshold (tune this)
+            self.get_logger().info("BLUE OBJECT DETECTED")
+
+            # Optional: save only when blue is detected
+            filename = os.path.join(self.output_dir, f"blue_{self.counter}.jpg")
+            cv2.imwrite(filename, frame)
+            self.counter += 1
 
 
 def main():
