67462ad1e5597ffad04d198a4d74ce1ff3d5f40b
MariaSotiriouSigma <sigoulakim@gmail.com>
2026-01-28 09:55:15 +0200

    velocity change

--- joystick_example/robot/robot/main.py
+++ joystick_example/robot/robot/main.py
@@ -79,7 +79,7 @@ class RobotController(Node):
 
     def move_forward(self):
         self.get_logger().info("Moving forward")
-        self.run_wheels('forward_callback', 0.5, 0.5)
+        self.run_wheels('forward_callback', 0.5, 0.4)
 
     def move_backward(self):
         self.get_logger().info("Moving backward")
